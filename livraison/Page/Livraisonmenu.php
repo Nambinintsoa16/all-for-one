330f48063e6ba1863ceefc7b0124fd65203cb43c
Andrysitraka <andrewsitraka@gmail.com>
2019-03-06 11:03:49 +0300

    integration page livraionmenu

--- livraison/Page/Livraisonmenu.php
+++ livraison/Page/Livraisonmenu.php
@@ -8,163 +8,197 @@ $date=$dt->format("Y-m-d");
     <section class="wrapper">
         <div class="row">
             <div class="col-lg-12">
-                <h3 class="page-header"><i class="fa fa-files-o"></i>Menu Livraison</h3>
+             
                 <ol class="breadcrumb">
                     <li><i class="fa fa-home"></i><a href="?page=">Accueil</a></li>
-                    <li><i class="fa fa-cube"></i><a href="#">Menu Livraison</a></li>                
+                    <li><i class="fa fa-cube"></i><a href="#">Menu Livraison</a></li>
                 </ol>
             </div>
         </div>
-        <div class="row">
-          <div class="col-md-12">
-               <img src="../img/livraison910.jpg" alt="" height="400" width="100%" style="background-size:cover;object-fit: cover;">
-          </div>
-            
+        <div class="row banniere_livraison">
+            <div class="col-md-12">
+                <img src="../img/livraison910.jpg" alt="" height="400" width="100%"
+                    class="bann_image">
+            </div>
+
         </div>
-        <div class="row" style="margin-top:30px;">
-          <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-          <a href="?page=livraison">
-            <div class="menubox" style="background-color:#03a5cc;min-height:150px; color: #fff">
-            <i class="fa fa-truck" style="z-index:3;position:absolute;font-size:50px;margin-top:30px;margin-left:150px;opacity:0.4"></i>
-               <div style="font-size: 28px; color: #fff;font-weight: bold;padding-top:30px;padding-left:20px">
-               <?php
-               $sql="SELECT `NumFact` FROM `facture` WHERE `datelivre`='".$date."'";
-               $resultfact=$main->fetchAll($sql);
-               if($resultfact){
-               foreach ($resultfact as $resultfact){
-                $factdouble[]=$resultfact['NumFact'];
-               }
-               $facture=array_unique($factdouble);
-               $nbfacture=count($facture);
-               echo $nbfacture;
-              }else{
-                $nbfacture=0;
-                echo $nbfacture;
-              }
-               ?>
-               </div>
-               <div style="padding-left:20px;margin-bottom:30px">Liste des Livraison </div>
-               <div style="z-index:#fff;height:30px;background:#038cae">
-                <span style="position:absolute;padding-top:5px !important;padding-left:20px;text-align:center">Plus d'info &nbsp; <i class="fa fa-arrow-circle-right"></i></span>
-              </div>
+        <div class="row menulivraison" >
+            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                <a href="?page=LivraisonEffectuée">
+
+                    <div class="bloc_menu">
+                        <div class="icon_block">
+                             <i class="fa fa-truck"></i>
+                        </div>
+                       
+                        <div class="block_nombre">
+                            <?php
+                                $sql="SELECT `NumFact` FROM `facture` WHERE `datelivre`='".$date."'";
+                                $resultfact=$main->fetchAll($sql);
+                                if($resultfact){
+                                foreach ($resultfact as $resultfact){
+                                  $factdouble[]=$resultfact['NumFact'];
+                                }
+                                $facture=array_unique($factdouble);
+                                $nbfacture=count($facture);
+                                echo $nbfacture;
+                                }else{
+                                  $nbfacture=0;
+                                  echo $nbfacture;
+                                }
+                            ?>
+                        </div>
+                        <div class="block_titre"> Livraison effectuée </div>
+                        <div class="plus_info">
+                           Plus d'info &nbsp; <i class="fa fa-arrow-circle-right"></i>
+                        </div>
+                    </div>
+                </a>
             </div>
+            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                <a href="?page=LivraisonConfirmée">
+                    <div class="bloc_menu1">
+                        <div class="icon_block1">
+                           <i class="fa fa-truck"></i> 
+                        </div>
+                        
+                        <div class="block_nombre1">
+                            <?php
+                                  $sql="SELECT * FROM `produit` ";
+                                  $count=$main->test($sql);
+                                  echo $count;
+                            ?>
+                        </div>
+                        <div class="block_titre1">Livraison confirmée</div>
+                        <div class="plus_info1">
+                          Plus  d'info &nbsp; <i class="fa fa-arrow-circle-right"></i>
+                        </div>
+                    </div>
+                    <a>
+
+            </div>
+
+
+            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                <a href="?page=LivraisonReporter">
+                    <div class="bloc_menu2">
+                        <div class="icon_block2">
+                            <i class="fa fa-truck"></i>
+                        </div>
+                        <div class="block_nombre2">
+                            <?PHP
+                              $sql="SELECT `id` FROM `client`";
+                              $countClient=$main->test($sql);
+                              echo  $countClient;
+                           ?>
+                        </div>
+
+                        <div class="block_titre2">Livraison raportée</div>
+                        <div class="plus_info2">
+                            Plusd'info &nbsp; <i class="fa fa-arrow-circle-right"></i>
+                        </div>
+                    </div>
+                    <a>
+            </div>
+
+
+            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                <a href="?page=LivraisonAnnulle">
+                    <div class="bloc_menu3">
+                        <div class="icon_block3">
+                            <i class="fa fa-truck"></i> 
+                        </div>
+                        <div class="block_nombre3">
+                            <?php
+                           $sql="SELECT `NumFact` FROM `facture` WHERE `datelivre`='".$date."'";
+                           $resultfact=$main->fetchAll($sql);
+                           if($resultfact){
+                           foreach ($resultfact as $resultfact){
+                            $factdouble[]=$resultfact['NumFact'];
+                           }
+                           $facture=array_unique($factdouble);
+                           $nbfacture=count($facture);
+                           echo $nbfacture;
+                          }else{
+                            $nbfacture=0;
+                            echo $nbfacture;
+                          }
+                           ?>
+                        </div>
+                        <div class="block_titre3"> livraison annulée</div>
+                        <div class="plus_info3">
+                            Plusd'info &nbsp; <i class="fa fa-arrow-circle-right"></i>
+                        </div>
+
+                    </div>
+            </div>
+
+             <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                <a href="?page=confirmationdelivraisons">
+                    <div class="bloc_menu5">
+                        <div class="icon_block5">
+                            <i class="fa fa-truck"></i> 
+                        </div>
+                        <div class="block_nombre5">
+                            <?php
+                           $sql="SELECT `NumFact` FROM `facture` WHERE `datelivre`='".$date."'";
+                           $resultfact=$main->fetchAll($sql);
+                           if($resultfact){
+                           foreach ($resultfact as $resultfact){
+                            $factdouble[]=$resultfact['NumFact'];
+                           }
+                           $facture=array_unique($factdouble);
+                           $nbfacture=count($facture);
+                           echo $nbfacture;
+                          }else{
+                            $nbfacture=0;
+                            echo $nbfacture;
+                          }
+                           ?>
+                        </div>
+                        <div class="block_titre5">Livraison à confirmer</div>
+                        <div class="plus_info5">
+                            Plusd'info &nbsp; <i class="fa fa-arrow-circle-right"></i>
+                        </div>
+
+                    </div>
+            </div>
+
+             <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                <a href="?page=Livresondujour">
+                    <div class="bloc_menu4">
+                        <div class="icon_block4">
+                            <i class="fa fa-calendar"></i> 
+                        </div>
+                        <div class="block_nombre4">
+                            <?php
+                           $sql="SELECT `NumFact` FROM `facture` WHERE `datelivre`='".$date."'";
+                           $resultfact=$main->fetchAll($sql);
+                           if($resultfact){
+                           foreach ($resultfact as $resultfact){
+                            $factdouble[]=$resultfact['NumFact'];
+                           }
+                           $facture=array_unique($factdouble);
+                           $nbfacture=count($facture);
+                           echo $nbfacture;
+                          }else{
+                            $nbfacture=0;
+                            echo $nbfacture;
+                          }
+                           ?>
+                        </div>
+                        <div class="block_titre4">Calendrier de livraison</div>
+                        <div class="plus_info4">
+                            Plusd'info &nbsp; <i class="fa fa-arrow-circle-right"></i>
+                        </div>
+
+                    </div>
+            </div>
+
+            
+
             </a>
-          </div>
-          <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-           <a href="?page=LivraisonAnnulle">
-            <div class="" style="background-color:#00a65a;min-height:150px; color: #fff" >
-              
-            <i class="fa fa-truck" style="z-index:3;position:absolute;font-size:50px;margin-top:30px;margin-left:150px;opacity:0.4"></i>
-        
-              <div style="font-size: 28px; color: #fff;font-weight: bold;padding-top:30px;padding-left:20px">
-             
-              <?php
-              $sql="SELECT * FROM `produit` ";
-              $count=$main->test($sql);
-              echo $count;
-              ?>
-              </div>
-              <div style="padding-left:20px;margin-bottom:30px">Annulée</div>
-              <div style="z-index:#fff;height:30px;background:#018d4e">
-                <span style="position:absolute;padding-top:5px !important;padding-left:20px;text-align:center">Plus d'info &nbsp; <i class="fa fa-arrow-circle-right"></i></span>
-              </div>
-            </div>
-            <a>
-          
-          </div>
+        </div>
+        </div>
        
-
-          <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-          <a href="?page=LivraisonEffectuée">
-            <div style="background-color:#f39c11;min-height:150px; color: #fff">
-            <i class="fa fa-truck" style="z-index:3;position:absolute;font-size:50px;margin-top:30px;margin-left:150px;opacity:0.4"></i>
         
-               <div style="font-size: 28px; color: #fff;font-weight: bold;padding-top:30px;padding-left:20px">
-              
-               <?PHP
-                  $sql="SELECT `id` FROM `client`";
-                  $countClient=$main->test($sql);
-                  echo  $countClient;
-
-               ?>
-               </div>
-               <div  style="padding-left:20px;margin-bottom:30px">Livrée</div>
-               <div style="z-index:#fff;height:30px;background:#d08510">
-                <span style="position:absolute;padding-top:5px !important;padding-left:20px;text-align:center">Plus d'info &nbsp; <i class="fa fa-arrow-circle-right"></i></span>
-              </div>
-            </div>
-            <a>
-          </div>
-          
-
-          <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-            <a  href="?page=confirmationdelivraisons">
-            <div style="background-color:#de4b39;min-height:150px; color: #fff">
-            <i class="fa fa-truck" style="z-index:3;position:absolute;font-size:50px;margin-top:30px;margin-left:150px;opacity:0.4"></i>
-        
-             
-            <div  style="font-size: 28px; color: #fff;font-weight: bold;padding-top:30px;padding-left:20px">
-            <?php
-               $sql="SELECT `NumFact` FROM `facture` WHERE `datelivre`='".$date."'";
-               $resultfact=$main->fetchAll($sql);
-               if($resultfact){
-               foreach ($resultfact as $resultfact){
-                $factdouble[]=$resultfact['NumFact'];
-               }
-               $facture=array_unique($factdouble);
-               $nbfacture=count($facture);
-               echo $nbfacture;
-              }else{
-                $nbfacture=0;
-                echo $nbfacture;
-              }
-               ?>
-          </div>
-            <div  style="padding-left:20px;margin-bottom:30px">Confirmer livraison</div>
-               <div style="z-index:#fff;height:30px;background:#bb4031">
-                <span style="position:absolute;padding-top:5px !important;padding-left:20px;text-align:center">Plus d'info &nbsp; <i class="fa fa-arrow-circle-right"></i></span>
-              </div>
-              
-            </div>
-          </div>
-        
-           </a>
-        </div> 
-            </div>
-<div class="row" style="margin-top:30px">
-          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" >
-          <a href="?page=Livresondujour">
-            <div style="background-color:#3c6980;min-height:150px; color: #fff">
-            <i class="fa fa-calendar" style="z-index:3;position:absolute;font-size:50px;margin-top:30px;margin-left:400px;opacity:0.4"></i>
-         
-               <div style="font-size: 28px; color: #fff;font-weight: bold;padding-top:30px;padding-left:20px">
-                  5
-               </div>
-               <div style="padding-left:20px;margin-bottom:50px">Calendrier</div>
-               <div style="z-index:#fff;height:30px;background:#33596c">
-                <span style="position:absolute;padding-top:5px !important;padding-left:20px;text-align:center">Plus d'info &nbsp; <i class="fa fa-arrow-circle-right"></i></span>
-              </div>
-            </div>
-            </a>
-          </div>
-          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-           <a href="?page=livraisondujour"> 
-            <div style="background-color:#00a38e;min-height:150px; color: #fff">
-            <i class="fa fa-truck" style="z-index:3;position:absolute;font-size:50px;margin-top:30px;margin-left:400px;opacity:0.4"></i>
-         
-              <div style="font-size: 28px; color: #fff;font-weight: bold;padding-top:30px;padding-left:20px">
-              <?php
-              $sql="SELECT * FROM `produit` ";
-              $count=$main->test($sql);
-              echo $count;
-              ?>
-              </div>
-              <div class="count"  style="padding-left:20px;margin-bottom:50px">Tout les livraison</div>
-              <div style="z-index:#fff;height:30px;background:#008a7a">
-                <span style="position:absolute;padding-top:5px !important;padding-left:20px;text-align:center">Plus d'info &nbsp; <i class="fa fa-arrow-circle-right"></i></span>
-              </div>
-            </div>
-            <a>
-          
-          </div>
-            </div>
